3679531d2cf2a6fbf0eb5c14a0bd32c33de66de9
preethi <preethisss2098@gmail.com>
2020-08-05 19:44:37 +0530

    initial commit

---
+++ src/test/java/com/dxc/faviarateservice1/Faviarateservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.faviarateservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Faviarateservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
